aad9c2e7d8cdfcc0d374405c7b53e2d621e02838
tacostrophe <tacokong@yandex.ru>
2023-08-24 09:05:21 +0300

    getting started

---
+++ db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "root",
+    DB: "testdb",
+    dialect: "postgres",
+  };
---
+++ models/keyboard.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => {
+  const Keyboard = sequelize.define('keyboard', {
+    type: {
+      type: Sequelize.STRING,
+      validate: {
+        is: /^(?:standard|inline)$/,
+      }
+    },
+    button_max_amount: {
+      type: Sequelize.INTEGER,
+    },
+    button_message_max_length: {
+      type: Sequelize.INTEGER,
+    },
+    link_button_support: {
+      type: Sequelize.BOOLEAN,
+    },
+    link_button_amount: {
+      type: Sequelize.INTEGER,
+    },
+  })
+
+  return Keyboard;  
+};
