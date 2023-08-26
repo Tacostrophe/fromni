1a8614fd763d3446a3754c6a9046dea77c50139f
tacostrophe <tacokong@yandex.ru>
2023-08-26 10:23:51 +0300

    name=> unique

--- app/models/canal.model.js
+++ app/models/canal.model.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, Sequelize) => {
   const Canal = sequelize.define('canal', {
     name: {
       type: Sequelize.STRING,
+      unique: true,
     },
     message_max_length: {
       type: Sequelize.INTEGER,
