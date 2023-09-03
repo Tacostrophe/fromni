5386837b14af945c2654cec8734c87a13baf6e30
tacostrophe <tacokong@yandex.ru>
2023-09-03 10:33:15 +0300

    adding convert and validation to campaigncreate

--- app/routes/campaign.routes.js
+++ app/routes/campaign.routes.js
@@ -1,10 +1,20 @@
 module.exports = () => {
-  const campaigns = require('../controllers/campaign.controller');
   const { Router } = require('express');
+
+  const campaigns = require('../controllers/campaign.controller');
+  const convertCampaign = require('../middlewares/convertCampaign');
+  const validate = require('../middlewares/validators');
+  const campaignValidationRules = require('../middlewares/validators/campaignValidationRules');
 
   const router = Router();
 
-  router.post('/create', campaigns.create);
+  router.get('/create', campaigns.createGet);
+  router.post(
+    '/create',
+    convertCampaign,
+    validate(campaignValidationRules),
+    campaigns.createPost
+  );
   router.get('/', campaigns.list);
   router.get('/:id', campaigns.retrieve);
 
