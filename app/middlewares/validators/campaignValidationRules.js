ba07399103b1d1e57d0edb6ce713c6d53b7b9ff0
tacostrophe <tacokong@yandex.ru>
2023-09-04 07:59:01 +0300

    added more detailed error messages

--- app/middlewares/validators/campaignValidationRules.js
+++ app/middlewares/validators/campaignValidationRules.js
@@ -2,20 +2,30 @@ const { custom, body } = require('express-validator');
 const db = require('../../models');
 
 const campaignValidationRules = [
-  body('campaigns', 'canal should be unique')
+  body('campaigns', 'Invalid input')
     .custom((value) => {
-      console.log(value);
-      const canals = value.reduce((acc, curr) => [...acc, curr.canal], []);
-      if (canals.length > new Set(canals).size) {
-        return Promise.reject();
+      if (value.length > 1) {
+        const canalsCount = value.reduce((acc, curr) => {
+          if (!acc[curr.canal]) {
+            acc[curr.canal] = 1;
+          } else {
+            acc[curr.canal]++;
+          }
+          return acc;
+        }, {});
+        const duplicateCanals = Object.keys(canalsCount).filter((canal) => canalsCount[canal] > 1);
+        if (duplicateCanals.length > 0) {
+          throw new Error(`Canals should be unique (${duplicateCanals.join(', ')})`);
+        }
       }
+      return true;
     }),
   body('campaigns.*.canal', 'that canal doesnt exist').trim()
     .custom(async(value, { req }) => {
       const canalObjects = await db.canals.findAll({ attributes: ['name'] });
       const canalNames = canalObjects.reduce((acc, current) => [...acc, current.name], []);
       if (!canalNames.includes(value)) {
-        return Promise.reject();
+        return Promise.reject(`Canal ${value} doesn't exist`);
       }
     }).escape(),
   body('campaigns.*.keyboard', 'keyboard type expected to be "standard" or "inline"')
@@ -26,27 +36,30 @@ const campaignValidationRules = [
       if (typeof value == 'string') {
         const { groups } = path.match(/^campaigns\[(?<campaignIndex>\d+)\].message$/);
         const campaignIndex = +groups.campaignIndex;
-        const canal = req.body.campaigns[campaignIndex].canal;
+        const campaign = req.body.campaigns[campaignIndex];
 
-        if (canal) {
+        // message length validation
+        if (campaign.canal) {
           const canalSetup = await db.canals.findOne({
-            where: { name: canal },
+            where: { name: campaign.canal },
             attributes: ['message_max_length'],
           });
           
           if (canalSetup.message_max_length
             && value.length > canalSetup.message_max_length) {
-            return Promise.reject(`Message max length: ${canalSetup.message_max_length}`);
+            return Promise.reject(`Message text for ${campaign.canal} with ${campaign.keyboard} keyboard`
+            + ` can't be longer than ${canalSetup.message_max_length}`);
           }
         }
       }
     }),
-  body('campaigns.*.buttons')
+  body('campaigns.*.buttons', 'Invalid buttons')
   .if((value, { req, path }) => value).isArray()
     .custom(async(value, { req, path }) => {
       if (Array.isArray(value)) {
         const { groups } = path.match(/^campaigns\[(?<campaignIndex>\d+)\].buttons$/);
         const campaignIndex = +groups.campaignIndex;
+
         const campaign = req.body.campaigns[campaignIndex];
         if (campaign.canal && ['standard', 'inline'].includes(campaign.keyboard)) {
           const canal = await db.canals.findOne({
@@ -56,15 +69,21 @@ const campaignValidationRules = [
           const keyboard = canal.keyboardInline ?? canal.keyboardStandard;
           // button amount validation
           if (value.length > +keyboard.button_max_amount) {
-            return Promise.reject(`Button amount can't be more than ${keyboard.button_max_amount}`);
+            return Promise.reject(
+              `Button amount for ${campaign.canal} with ${campaign.keyboard} keyboard`
+              + ` can't be more than ${keyboard.button_max_amount}`
+            );
           }
           // link-button amount validation
           const linkButtonAmount = value.reduce((acc, curr) => (curr.type == 'link' ? acc+1 : acc), 0);
           if (isFinite(+keyboard.link_button_amount)) {
             if ((+keyboard.link_button_amount) == 0 && linkButtonAmount > 0) {
-              return Promise.reject('link-buttons are\'not supported');
+              return Promise.reject(`Link-buttons are not supported for ${campaign.canal} with ${campaign.keyboard} keyboard`);
             } else if ((+keyboard.link_button_amount) < linkButtonAmount) {
-              return Promise.reject(`link-button amount can't be more than ${keyboard.link_button_amount}`);
+              return Promise.reject(
+                `Link-button amount for ${campaign.canal} with ${campaign.keyboard} keyboard`
+                + ` can't be more than ${keyboard.link_button_amount}`
+              );
             }
           }
         }
@@ -83,7 +102,8 @@ const campaignValidationRules = [
         const keyboard = canal.keyboardInline ?? canal.keyboardStandard;
         if (keyboard.button_message_max_length
           && keyboard.button_message_max_length < value.length) {
-          return Promise.reject(`Button text can't be longer than ${keyboard.button_message_max_length}`);
+          return Promise.reject(`Button text for ${campaign.canal} with ${campaign.keyboard} keyboard`
+          + ` can't be longer than ${keyboard.button_message_max_length}`);
         }
       }
     }),
