51690f6f8b5351881f88647ce0bd856e0a438786
tacostrophe <tacokong@yandex.ru>
2023-09-15 14:18:44 +0300

    getting rid of convertCampaign

--- app/routes/campaign.routes.js
+++ app/routes/campaign.routes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 
 const campaigns = require('../controllers/campaign.controller');
-const convertCampaign = require('../middlewares/convertCampaign');
 const validate = require('../middlewares/validators');
 const campaignValidationRules = require('../middlewares/validators/campaignValidationRules');
 
@@ -11,7 +10,6 @@ module.exports = () => {
   router.get('/create', campaigns.createGet);
   router.post(
     '/create',
-    convertCampaign,
     validate(campaignValidationRules),
     campaigns.createPost,
   );
